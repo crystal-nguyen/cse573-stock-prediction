3af500eea660c49fe37e04312ceaf85666d40d13
bnguye23 <47970344+bnguye23@users.noreply.github.com>
2022-04-12 16:06:53 -0700

    Extracted tweet text to global var

--- interface/main.js
+++ interface/main.js
@@ -1,8 +1,32 @@
 document.getElementById("getTweetButton").onclick = function() { predictStockDirection() };
 
 function predictStockDirection() {
+	var xhr = new XMLHttpRequest();
+	// ### Change endpoint ip to localhost:5000 ###
+	var predict_endpoint = "http://192.168.0.199:5000/predict";
+	xhr.open("POST", predict_endpoint);
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.send(JSON.stringify({
+	    tweetURL: document.getElementById("tweetText").value
+	}));
+	xhr.onreadystatechange=(e)=>{
+		/*
+		if (jsonResponse['direction'] == 'up') {
+			var arrow = document.getElementById('up_arrow');
+			arrow.style.visibility = ('visible');
+		} else {
+			var arrow = document.getElementById('down_arrow');
+			arrow.style.visibility = ('visible');
+		}
+		*/
+	}
+}
+
+function getTweets() {
 var xhr = new XMLHttpRequest();
-	var predict_endpoint = "http://10.0.2.15:5000/tweets";
+	// ### Change endpoint ip to localhost ###
+	var predict_endpoint = "http://192.168.0.199:5000/tweets";
 	xhr.open("POST", predict_endpoint);
 	xhr.setRequestHeader("Accept", "application/json");
 	xhr.setRequestHeader("Content-Type", "application/json");
@@ -12,5 +36,6 @@ var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange=(e)=>{
 		var jsonResponse = JSON.parse(xhr.responseText);
 		document.getElementById('tweetResult').innerHTML = jsonResponse['result'];
+		// console.log(jsonResponse['result']);
 	}
 }
--- server/server.py
+++ server/server.py
@@ -6,13 +6,14 @@ import numpy as np
 import json
 import random
 import requests
-import twint
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
 class server():
+    tweetText = ""
+
     def __init__(self):
         # initializing flask
         self.app = Flask(__name__)
@@ -24,7 +25,8 @@ class server():
         self.app.add_url_rule("/tweets", view_func=self.get_tweets, methods=["POST"])
         
         # starting flask server
-        self.host = "10.0.2.15"
+        ### change self.host ip to localhost ####
+        self.host = "192.168.0.199"
         self.app.run(host=self.host)
 
     # Function to call for "/predict" route
@@ -34,12 +36,43 @@ class server():
             tweetURL = data["tweetURL"]
             options = Options()
             options.headless = True
-            CHROMEDRIVER_PATH = "/usr/lib/chromium-browser/chromedriver"
+
+            ### Change chrome driver path ###
+
+            # Joseph's
+            # CHROMEDRIVER_PATH = "/usr/lib/chromium-browser/chromedriver"
+            # Brian's
+            CHROMEDRIVER_PATH = "/opt/homebrew/bin/chromedriver"
+
             driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
             driver.get('https://publish.twitter.com/?query=' + tweetURL)
             soup = BeautifulSoup(driver.page_source, "lxml")
             item = soup.find('code', attrs={"class":"EmbedCode-code"})
             embed = item.contents[0]
+            response = {"result": f"{embed}"}
+            response = json.dumps(response)
+            return Response(response=response, status=200, mimetype="application/json")
+        
+    def get_tweets(self):
+        if request.method == "POST":
+            data = request.get_json(force=True)
+            tweetURL = data["tweetURL"]
+            options = Options()
+            options.headless = True
+
+            ### Change chrome driver path ###
+
+            # Joseph's
+            # CHROMEDRIVER_PATH = "/usr/lib/chromium-browser/chromedriver"
+            # Brian's
+            CHROMEDRIVER_PATH = "/opt/homebrew/bin/chromedriver"
+            driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
+            driver.get('https://publish.twitter.com/?query=' + tweetURL)
+            soup = BeautifulSoup(driver.page_source, "lxml")
+            item = soup.find('code', attrs={"class":"EmbedCode-code"})
+            text = soup.find_all("p")[-1].get_text()
+            embed = item.contents[0]
+            tweetText = text
             response = {"result": f"{embed}"}
             response = json.dumps(response)
             return Response(response=response, status=200, mimetype="application/json")
