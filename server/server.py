f8c3d2a31a6247dfc24b4d0ae397eff189c3b7da
crystal-nguyen <crystalktnguyen@gmail.com>
2022-04-12 17:01:43 -0700

    added functions to input data and trained models

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 import base64
+from cmath import exp
 import os
 from flask import Flask, request, Response, jsonify
 from flask_cors import CORS
@@ -11,6 +12,17 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import emoji
+import re
+import contractions
+import pickle
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.preprocessing import StandardScaler
+
+
+
 
 class server():
     def __init__(self):
@@ -59,6 +71,123 @@ class server():
             response = {"result": f"{embed}"}
             response = json.dumps(response)
             return Response(response=response, status=200, mimetype="application/json")
-    
+
+    def predict_model1(text):
+        sid = SentimentIntensityAnalyzer()
+        scaler = StandardScaler()
+
+        # load in trained logistic regression model
+        file = open('lr.sav','rb')
+        model = pickle.load(file)
+        file.close()
+
+        # load in trained w2v model
+        w2v_file = open('w2v_model.sav','rb')
+        w2v_model = pickle.load(w2v_file)
+        w2v_file.close()
+
+        # create the feature vector
+        words_set = set(w2v_model.wv.index_to_key)
+        tokens = text.split()
+        X_vect = np.array([w2v_model.wv[i] for i in tokens if i in words_set])
+        X_vect_avg = []
+        if X_vect.size:
+            X_vect_avg.append(np.append(X_vect.mean(axis=0),sid.polarity_scores(text)['compound']))
+        else:
+            X_vect_avg.append(np.append(np.zeros(100, dtype=float),sid.polarity_scores(text)['compound']))
+        X_vect_scale = scaler.fit_transform(X_vect_avg)
+
+        # run prediction on trained model
+        prediction = model.predict(X_vect_scale)[0]
+        
+        return prediction
+
+    def predict_model2(text):
+        pass
+
+
+    def predict_model3(text):
+        sid = SentimentIntensityAnalyzer()
+        scaler = StandardScaler()
+
+        # topic model
+        tm_file = open('topic_model.sav','rb')
+        tm_model = pickle.load(tm_file)
+        tm_file.close()
+
+        # words
+        word_file = open('tm_words.sav','rb')
+        words = pickle.load(word_file)
+        word_file.close()
+
+        # id2word
+        id2word_file = open('tm_id2word.sav','rb')
+        id2word = pickle.load(id2word_file)
+        id2word_file.close()
+
+        # corpus
+        corpus_file = open('tm_corpus.sav','rb')
+        corpus = pickle.load(corpus_file)
+        corpus_file.close()
+
+        # create feature vector 
+        split_text = text.split()
+        new_text = id2word.doc2bow(split_text)
+        topics = tm_model.get_document_topics(new_text,
+                                            minimum_probability=0.0)
+        features = [topics[i][1] for i in range(70)]
+        features.append(sid.polarity_scores(text)['compound'])
+        X_vect_scale = scaler.fit_transform([features])
+
+        # run prediction on trained model
+        file = open('lr_topic_model.sav','rb')
+        model = pickle.load(file)
+        file.close()
+        prediction = model.predict(X_vect_scale)[0]
+
+        return prediction
+
+    def preprocess_text(text):
+        # lowercase
+        preprocess = text.lower()
+        # remove emoji
+        preprocess = emoji.get_emoji_regexp().sub(u'',preprocess)
+        # remove new line
+        preprocess = re.sub('\n',' ',preprocess)
+        # replace with tokens
+        preprocess = re.sub('http\S+', 'LINK', preprocess)
+        preprocess = re.sub('\B\#\w+','HASHTAG',preprocess)
+        preprocess = re.sub('\B\@\w+','AMPERSAND',preprocess)
+        preprocess = re.sub('\B\$\w+','DOLLARSIGN',preprocess)
+
+        # expand contractions
+        temp = preprocess.split()
+        expanded_text = []
+        for word in temp:
+            expanded_text.append(contractions.fix(word))
+        preprocess = ' '.join(expanded_text)
+
+        # remove extra whitespace
+        preprocess = re.sub(r'[^\w\s]',' ',preprocess)
+        preprocess = re.sub(' +', ' ',preprocess)
+        preprocess = preprocess.strip()
+
+        return preprocess
+
+    def predict_direction(text):
+            preprocess = preprocess_text(text)
+            
+            # model 1 prediction
+            prediction_1 = predict_model1(preprocess)
+        
+            #model 2
+            prediction_2 = predict_model2(preprocess)
+
+            #model 3
+            prediction_3 = predict_model3(preprocess)
+
+            return [prediction_1,prediction_2,prediction_3]
+
+
 if __name__ == "__main__":
     server()
